7a26c32029ceaab5e34c502e84d14f54d7e0d29f
sid995 <sk31395@gmail.com>
2018-10-20 12:49:23 +0530

    Setting up Navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
+import NavBar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>BlogPost</h1>
+          <NavBar />
         </div>
       </BrowserRouter>
     );
