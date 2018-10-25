0de9b00e57d10f21e32953d6c0509a001621eff3
Siddharth Kundu <sk31395@gmail.com>
2018-10-25 22:32:06 +0530

    created firebase config file

---
+++ src/config/fbConfig.js
@@ -0,0 +1,16 @@
+import { firebase } from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
+var config = {
+  apiKey: "AIzaSyAI7a7ao3mL87_rDfLJ-iQCVrJ6M5BonKw",
+  authDomain: "marioplan-4e2aa.firebaseapp.com",
+  databaseURL: "https://marioplan-4e2aa.firebaseio.com",
+  projectId: "marioplan-4e2aa",
+  storageBucket: "marioplan-4e2aa.appspot.com",
+  messagingSenderId: "942420480163"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
