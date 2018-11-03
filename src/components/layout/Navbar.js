99eeeac541050903c8a763a0a41b4a146721a49f
Siddharth Kundu <sk31395@gmail.com>
2018-11-03 12:48:03 +0530

    Created user profile data

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,9 +5,13 @@ import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
 
 const NavBar = props => {
-  const { auth } = props;
+  const { auth, profile } = props;
   // console.log(auth);
-  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+  const links = auth.uid ? (
+    <SignedInLinks profile={profile} />
+  ) : (
+    <SignedOutLinks />
+  );
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
@@ -21,8 +25,10 @@ const NavBar = props => {
 };
 
 const mapStateToProps = state => {
+  console.log(state);
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
   };
 };
 
