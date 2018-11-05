6b35a131f013e87574d8e6bb6db225359142e0fa
Siddharth Kundu <sk31395@gmail.com>
2018-11-06 01:46:11 +0530

    Created initial notification list

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Notifications = () => {
-  return <p>Notifications</p>;
+const Notifications = props => {
+  return (
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+          <ul className="notifications">
+            <li>Notifications</li>
+            <li>Notifications</li>
+            <li>Notifications</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Notifications;
